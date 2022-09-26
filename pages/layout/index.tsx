e2ab1d194d0455fb85af5ca56b17e67c8fd102b2
rastaxarm <xarmzon@gmail.com>
2022-09-26 19:28:41 +0000

    update: components

--- pages/layout/index.tsx
+++ pages/layout/index.tsx
@@ -12,7 +12,7 @@ const Layout = ({ title, children }: ILayout) => {
     <>
       {title && <NextSeo title={title} />}
       <Header />
-      <div className="w-full mt-16 py-5 px-5 bg-circuit_bg bg-center bg-contain overflow-x-hidden min-h-[calc(100vh-128px-16px)] grid place-items-center">
+      <div className="w-full mt-10 py-5 px-5 bg-circuit_bg bg-center bg-contain overflow-x-hidden min-h-[calc(100vh-128px-16px)] grid place-items-center">
         <div className="w-full overflow-x-hidden">{children}</div>
       </div>
       <Footer />
